e2ec28c40574a51919f3a3a767709a4fd027e698
Baptiste Ribot <baptisteribot@outlook.fr>
2023-05-11 14:28:34 +0200

    begining cards

---
+++ src/components/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./card.css";
+
+function Card() {
+    return (
+        <figure className='figure-card'>
+            <figcaption className='card-title'>Titre de la location</figcaption>
+        </figure>
+    );
+  }
+  
+  export default Card;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import Banner from "../components/Banner";
+import Card from "../components/Card";
 import "./home.css";
 
 const Home = () => {
     return (
       <div className="div-home">
         <Banner />
+        <Card />
       </div>
     );
   };
